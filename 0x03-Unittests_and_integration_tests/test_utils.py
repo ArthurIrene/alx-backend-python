108ab59004e7ef8d07eccb965a9ea25e5a803e57
Arthur <shingiroirene@gmail.com>
2025-05-31 18:41:39 +0200

    Test with errors

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,7 +6,7 @@ from utils import access_nested_map
 class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}},("a",),{"b": 2}),
+        ({"a": {"b": 2}},("a",),{"b": 2}),  
         ({"a": {"b": 2}},("a", "b"),2)      
         ])
     def test_access_nested_map(self,nested_map, path , result):
@@ -14,3 +14,13 @@ class TestAccessNestedMap(unittest.TestCase):
 
         self.assertEqual(actual_value, result)
         pass 
+    @parameterized.expand([
+        ({}, ("a",) , 'a'),
+        ({"a": 1}, ("a", "b"), 'b')
+    ])
+    def test_access_nested_map_exception(self,nested_map, path , result):
+        with self.assertRaises(KeyError) as context_manager:
+            access_nested_map(nested_map, path)
+
+        self.assertEqual(context_manager.exception.args[0], result)
+        pass    
